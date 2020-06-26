9c2cbcdf90d6711f51f4002ef5dad0b81d61e298
Ottoyd <ugaipribadi@gmail.com>
2020-06-26 13:05:09 +0700

    Waiting for Padding and Margin Detail

--- App.js
+++ App.js
@@ -1,86 +1,94 @@
-import React from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, ScrollView, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {Platform, StyleSheet, Text, View, Image, TextInput, ScrollView, TouchableOpacity, Button} from 'react-native';
+import styles from './loginStyle'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.scroll}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <View>
-          <View style = {styles.ContainerTitle}>
-            <Image
-              style={styles.tinyLogo}
-              source={{
-                uri: 'https://img.freepik.com/free-vector/golden-retro-restaurant-logo-set_23-2148370377.jpg?size=626&ext=jpg',
-              }}
-            />
-          </View>
-          <View style={{flexDirection:'row', justifyContent:'space-between',marginBottom:16,paddingHorizontal:16}}>
-            <Text style={{fontSize:17, fontWeight:'bold', color:'#1C1C1C'}}>Info Tanaman</Text>
-            <Text style={{fontSize:17, fontWeight:'bold', color:'#61A756'}}>See All</Text>
-          </View>
-        </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LandingPage">
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
 
-        <ScrollView horizontal style={{flexDirection:'row'}}>
-        <View style={{marginRight:16}}>
-            <View style={{width:150, height:150, borderRadius:10}}>
-              <Image
-                style={styles.tinyLogo}
-                source={{
-                  uri: 'https://img.freepik.com/free-vector/golden-retro-restaurant-logo-set_23-2148370377.jpg?size=626&ext=jpg',
-                }}
-              />
-            </View>
-              <Text style={{fontSize:16, fontWeight:'bold', color:'#1C1C1C',marginTop:12}}>Dummy tanamanew</Text>
-          </View>
 
-          <View style={{marginRight:16}}>
-            <View style={{width:150, height:150, borderRadius:10}}>
-              <Image
-                style={styles.tinyLogo}
-                source={{
-                  uri: 'https://img.freepik.com/free-vector/golden-retro-restaurant-logo-set_23-2148370377.jpg?size=626&ext=jpg',
-                }}
-              />
-            </View>
-              <Text style={{fontSize:16, fontWeight:'bold', color:'#1C1C1C',marginTop:12}}>Dummy tanamanew</Text>
-          </View>
+function LandingPage({ navigation }) {
+  const [register, setRegister] = useState(false);
+  const [textRegLog, setText] = useState('Didnt Have Account ?');
+  const regLogHandle = function() {
+    setRegister(!register)
+    if (register) setText('Already Have Account ?')
+    if (!register) setText('Didnt Have Account ?')
+  }
 
-          <View style={{marginRight:16}}>
-            <View style={{width:150, height:150, borderRadius:10}}>
-              <Image
-                style={styles.tinyLogo}
-                source={{
-                  uri: 'https://img.freepik.com/free-vector/golden-retro-restaurant-logo-set_23-2148370377.jpg?size=626&ext=jpg',
-                }}
-              />
-            </View>
-              <Text style={{fontSize:16, fontWeight:'bold', color:'#1C1C1C',marginTop:12}}>Dummy tanamanew</Text>
-          </View>
-        </ScrollView>
-        <View style={{paddingBottom:8, borderBottomColor: '#E8E9ED', borderBottomWidth:1, marginTop:16,marginHorizontal:16, marginBottom:20}}></View>
-      </ScrollView>
+  const isRegister = function() {
+    if (register) {
+      return (
+        <TextInput style={styles.inputBox}
+          placeholder="Confirm Password"
+          placeholderTextColor = "#ffffff"
+          selectionColor="#fff"
+          keyboardType="confirm-password"
+          onSubmitEditing={()=> this.password.focus()}
+        />
+      )
+    }
+  }
+
+  const btnHandle = function() {
+    if (!register) {
+      navigation.navigate('HomePage')
+    }
+  }
+
+  return (
+      <View style={styles.container}>
+        <TextInput style={styles.inputBox}
+          placeholder="Email"
+          placeholderTextColor = "#ffffff"
+          selectionColor="#fff"
+          keyboardType="email-address"
+          onSubmitEditing={()=> this.password.focus()}
+        />
+
+        <TextInput style={styles.inputBox}
+          placeholder="Password"
+          placeholderTextColor = "#ffffff"
+          selectionColor="#fff"
+          keyboardType="password"
+          onSubmitEditing={()=> this.password.focus()}
+        />
+
+        {isRegister()}
+
+        <TouchableOpacity style={styles.button} onPress={regLogHandle}>
+          <Text>{textRegLog}</Text>
+        </TouchableOpacity>
+
+        <Button
+          title={register ? 'Register' : 'Login'}
+          onPress={btnHandle}
+        />
+      </View>
+  );
+}
+
+function HomePage() {
+  return (
+    <View>
+      <Text>HomePage</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  scroll: {
-    flex: 1,
-    backgroundColor: 'white',
-    paddingTop:15
-  },
-  ContainerTitle: {
-    height:15,
-    width:60,
-    marginLeft: 16
-  },
-  tinyLogo: {
-    width:undefined,
-    height:undefined,
-    resizeMode:'contain',
-    flex:1
-  },
-});
+
+
+
+
+
--- loginStyle.js
+++ loginStyle.js
@@ -2,11 +2,23 @@ import {StyleSheet} from 'react-native'
 
 export default StyleSheet.create({
 
-container: {
+  container: {
     flex: 1,
-    marginTop: 20
-},
-welcome: {
+    backgroundColor: 'grey',
+  },
+
+  welcome: {
     textAlign: 'center'
-}
+  },
+
+  inputBox: {
+    width:300,
+    backgroundColor:'rgba(255, 255,255,0.2)',
+    borderRadius: 25,
+    paddingHorizontal:16,
+    fontSize:16,
+    color:'#ffffff',
+    marginVertical: 10
+  },
+
 });
