de8e068b54ea9ffd3a3856bb728892b81cc244eb
Ottoyd <ugaipribadi@gmail.com>
2020-06-27 21:25:15 +0700

    '70%'

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingPage from './src/views/LandingPage'
 import HomePage from './src/views/HomePage'
+import ProfilePage from './src/views/ProfilePage'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomePage">
+      <Stack.Navigator initialRouteName="ProfilePage">
         <Stack.Screen name="LandingPage" component={LandingPage} />
         <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="ProfilePage" component={ProfilePage} />
       </Stack.Navigator>
     </NavigationContainer>
   )
