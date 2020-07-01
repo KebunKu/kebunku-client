26b5e0fb86d776b1321679f01c8267b3fee053c8
hmyslf <yosa.alfq4@gmail.com>
2020-07-01 21:07:50 +0700

    add delete & modal confirmation, add condition rendering image

--- src/store/actions/deleteUserPlant.js
+++ src/store/actions/deleteUserPlant.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
 import {fetchAllMyPlant} from './getUserPlant.js'
 import { AsyncStorage } from 'react-native';
-
+import server from '../config.js';
 export const DELETE_USER_PLANT = 'DELETE_USER_PLANT';
 
-export const deleteFavorite = (id) => {
-  
+export const deleteUserPlant = (id) => {
+  console.log(id, 'dari deleteuser plant nih!!!')
   return (dispatch) => {
     AsyncStorage.getItem('token', (err, result) => {
-      axios({
+      server({
         method: 'DELETE',
-        url: `http://192.168.43.189:3000/userplant/${id}`,
+        url: `/userplant/${id}`,
         headers: {
           token: result
         }
       })
       .then(({ data }) => {
+        console.log(data);
         dispatch(fetchAllMyPlant())
       })
       .catch((error) => {
